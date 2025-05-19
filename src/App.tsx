b1740210fe0cd16cc422f4648bcbdba140c5ffeb
Justin Graham <Justin.af.graham@gmail.com>
2025-05-19 10:36:03 -0700

    Add AppProvider to wrap components using context

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { AppProvider } from './context/AppContext';
 import PrivateRoute from './components/PrivateRoute';
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
@@ -13,34 +14,38 @@ function App() {
   if (isStudentWindow) {
     return (
       <AuthProvider>
-        <StudentDisplayPage />
+        <AppProvider>
+          <StudentDisplayPage />
+        </AppProvider>
       </AuthProvider>
     );
   }
 
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/classroom/:sessionId"
-            element={
-              <PrivateRoute>
-                <MainPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <Navigate to="/classroom/new" replace />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+      <AppProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/classroom/:sessionId"
+              element={
+                <PrivateRoute>
+                  <MainPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Navigate to="/classroom/new" replace />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </AppProvider>
     </AuthProvider>
   );
 }
