07aee1c7eb0671b4c3ac9fbb498be135dc24c076
Justin Graham <justin.af.graham@gmail.com>
2025-08-01 10:55:56 -0700

    Fixed typing

--- src/core/content/SlideContent15.ts
+++ src/core/content/SlideContent15.ts
@@ -1,5 +1,5 @@
 // src/core/content/SlideContent15.ts
-import {Slide} from '@shared/types/game';
+import {Slide, SlideType} from '@shared/types/game';
 import {getFilteredSlides} from './GameStructure';
 
 /**
@@ -97,6 +97,7 @@ const createDecimalSlides = (): Slide[] => [
 export const get15Slides = (): Slide[] => {
     const baseSlides: Slide[] = getFilteredSlides('2.0_no_dd');
     const decimalSlides: Slide[] = createDecimalSlides();
+    const image: SlideType = 'image' as SlideType
 
     return baseSlides.map(slide => {
         // Override specific slides for version 1.5
@@ -105,220 +106,224 @@ export const get15Slides = (): Slide[] => {
                 return {
                     ...slide,
                     title: "Welcome",
-                    source_path: 'Slide_001.jpg'
+                    source_path: 'Slide_001.jpg',
+                    type: image,
                 };
             case 1:
                 return {
                     ...slide,
                     title: "Overview",
-                    source_path: 'Slide_002.jpg'
+                    source_path: 'Slide_002.jpg',
+                    type: image,
                 };
             case 2:
                 return {
                     ...slide,
                     title: "Target Acquisition",
-                    source_path: 'Slide_003.jpg'
+                    source_path: 'Slide_003.jpg',
+                    type: image,
                 };
             case 3:
                 return {
                     ...slide,
                     title: "Target Acquisition",
-                    source_path: 'Slide_004.jpg'
+                    source_path: 'Slide_004.jpg',
+                    type: image,
                 };
             case 4:
                 return {
                     ...slide,
                     title: "Mission Briefing",
                     source_path: 'Slide_005.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 5:
                 return {
                     ...slide,
                     title: "Welcome to ALU",
                     source_path: 'Slide_006.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 6:
                 return {
                     ...slide,
                     title: "How to Run",
                     source_path: 'Slide_007.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 8:
                 return {
                     ...slide,
                     source_path: 'Slide_009.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 9:
                 return {
                     ...slide,
                     source_path: 'Slide_010.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 11:
                 return {
                     ...slide,
                     source_path: 'Slide_012.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 12:
                 return {
                     ...slide,
                     source_path: 'Slide_013.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 13:
                 return {
                     ...slide,
                     source_path: 'Slide_014.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 14:
                 return {
                     ...slide,
                     source_path: 'Slide_015.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 15:
                 return {
                     ...slide,
                     source_path: 'Slide_016.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 16:
                 return {
                     ...slide,
                     source_path: 'Slide_017.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 18:
                 return {
                     ...slide,
                     source_path: 'Slide_019.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 19:
                 return {
                     ...slide,
                     source_path: 'Slide_020.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 20:
                 return {
                     ...slide,
                     source_path: 'Slide_021.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 21:
                 return {
                     ...slide,
                     source_path: 'Slide_022.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 22:
                 return {
                     ...slide,
                     source_path: 'Slide_023.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 23:
                 return {
                     ...slide,
                     source_path: 'Slide_024.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 24:
                 return {
                     ...slide,
                     source_path: 'Slide_025.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 25:
                 return {
                     ...slide,
                     source_path: 'Slide_026.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 27:
                 return {
                     ...slide,
                     source_path: 'Slide_028.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 28:
                 return {
                     ...slide,
                     source_path: 'Slide_029.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 29:
                 return {
                     ...slide,
                     source_path: 'Slide_030.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 30:
                 return {
                     ...slide,
                     source_path: 'Slide_031.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 31:
                 return {
                     ...slide,
                     source_path: 'Slide_032.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 33:
                 return {
                     ...slide,
                     source_path: 'Slide_034.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 34:
                 return {
                     ...slide,
                     source_path: 'Slide_035.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 35:
                 return {
                     ...slide,
                     source_path: 'Slide_036.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 36:
                 return {
                     ...slide,
                     source_path: 'Slide_037.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 37:
                 return {
                     ...slide,
                     source_path: 'Slide_038.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 38:
                 return {
                     ...slide,
                     source_path: 'Slide_039.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 39:
                 return {
                     ...slide,
                     source_path: 'Slide_040.jpg',
-                    type: 'image',
+                    type: image,
                 };
             case 40:
                 return {
                     ...slide,
                     source_path: 'Slide_041.jpg',
-                    type: 'image',
+                    type: image,
                 };
             default:
                 return slide;
