b4597e67ef9fd79cf70dbb456004483e159b6350
Derek Paniagua <paniaguaderek@gmail.com>
2025-10-28 17:37:12 -0600

    Replaced display version for completed games

--- src/views/host/pages/DashboardPage.tsx
+++ src/views/host/pages/DashboardPage.tsx
@@ -449,7 +449,7 @@ const DashboardPage: React.FC = () => {
                                                                     <div>
                                                                         <span
                                                                             className="font-medium text-gray-700">Version:</span>
-                                                                        <div>v{game.game_version.startsWith('2') ? '2.0' : '1.5'}</div>
+                                                                        <div>{GameVersionManager.getDisplayVersion(game.game_version)}</div>
                                                                     </div>
                                                                 )}
                                                                 <div>
