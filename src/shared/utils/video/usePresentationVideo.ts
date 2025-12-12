dfc6dd7319dec02fb11b4de53bedd66dfc98412f
Derek Paniagua <paniaguaderek@gmail.com>
2025-12-12 13:57:45 -0700

    removed pause logic no longer needed

--- src/shared/utils/video/usePresentationVideo.ts
+++ src/shared/utils/video/usePresentationVideo.ts
@@ -420,10 +420,6 @@ export const usePresentationVideo = ({ sessionId, sourceUrl, isEnabled }: UsePre
 
         if (sourceUrl) {
             if (video.currentSrc !== sourceUrl) {
-                if (!video.paused) { // Pause before changing to avoid two audio streams playing at once
-                    video.pause();
-                }
-
                 video.src = sourceUrl;
                 video.load();
                 
